fdc089e0f47f51a83259a626c3cd1a814914f20b
Imad <65905842+Tech-Sec@users.noreply.github.com>
2020-09-17 10:31:53 +0300

    Add files via upload

---
+++ InfoG.py
@@ -0,0 +1,24 @@
+from utils import *
+tool_name()
+your_ip()
+print('''
+[1]Get website IP address
+
+[2]IP lookup
+
+[3]Phone no. lookup
+
+''')
+try:
+    command= input('InfoG> ')
+    if command == '1':
+        domain_name()
+    elif command == '2':
+        ip_lookup()
+    elif command == '3':
+        ph_num_lookup()
+    else:
+        print('Enter a valid command')
+
+except KeyboardInterrupt:
+    tool_name()
---
+++ utils.py
@@ -0,0 +1,53 @@
+import socket
+import phonenumbers
+import requests
+from phonenumbers import geocoder
+
+def tool_name():
+    print('''
+ ___                                               ____________
+|%%%|                                             |@@@@@@@@@@@@|
+|%%%|  ____       ___   _________   ____________  |@@@@________|       
+|%%%| |%%%%\     |%%%| |%%% _____| |%%%______%%%| |@@@|
+|%%%| |%%%%%\    |%%%| |%%%|_____  |%%|      |%%| |@@@|       ___
+|%%%| |%%%%%%\   |%%%| |%%%_____|  |%%|      |%%| |@@@|      |@@@|
+|%%%| |%%%|\%%\  |%%%| |%%%|       |%%|      |%%| |@@@|______|@@@|
+|%%%| |%%%| \%%\ |%%%| |%%%|       |%%|______|%%| |@@@@@@@@@@@@@@|
+|___| |___|  \_______| |___|       |____________| |______________|By: Tech-Sec
+For more visit: https://github/Tech-Sec
+''')
+
+
+def domain_name():
+    print(socket.gethostbyname(input('Domain name: ')))
+
+
+def your_ip():
+    your_ip=requests.get('https://geolocation-db.com/json/').json() 
+    print('Your public IP address: ',your_ip.get('IPv4'))
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    print('Your device name: ',hostname)
+    print('Your private IP address ',ip_address)
+
+
+def ip_lookup():
+    enter_ip = input('IP Address: ')
+    response = requests.get(f"https://geolocation-db.com/json/{enter_ip}&position=true").json()
+    for key, value in response.items():
+        print(key, ' : ', value)
+
+
+def ph_num_lookup():
+    your_ph_num = input('Your Number: ')
+    phone_number = phonenumbers.parse(your_ph_num)
+    # put your phonenumber with the country code eg:+1xxxxxxxx
+    print(geocoder.description_for_number(phone_number,
+                                          'en'))
+
+
+    from phonenumbers import carrier
+    service_provider = phonenumbers.parse(your_ph_num)
+    # put your phonenumber with the country code eg:+1xxxxxxxx
+    print(carrier.name_for_number(service_provider,
+                                  'en'))
